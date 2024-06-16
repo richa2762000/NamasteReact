d843114009a765c3eadc21f91fe44880c4df0f0f
richa_chachan <richa.chachan@decimal.co.in>
2024-06-16 16:17:07 +0530

    oldReactMethod

---
+++ App.js
@@ -0,0 +1,47 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+// single tag
+const heading1 = React.createElement("h1", { id: "heading" }, "Hi");
+// root.render(heading);
+
+
+// nested child div
+const nestedHead = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "head" }, "I'm child")
+  )
+);
+// root.render(nestedHead);
+
+
+// simbling tag - for simbling we will use array
+const simblingTag = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "head1" }, "haed1"),
+    React.createElement("h2", { id: "head2" }, "head2"),
+  ])
+);
+// root.render(simblingTag);
+
+
+// simbling div tag - for simbling we will use array
+const simblingDivTag = React.createElement(
+    "div",
+    { id: "parent" },
+   [ React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "head1" }, "haed1"),
+    React.createElement("h2", { id: "head2" }, "head2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "head1" }, "haed1"),
+    React.createElement("h2", { id: "head2" }, "head2"),
+  ])]
+  );
+  root.render(simblingDivTag);
