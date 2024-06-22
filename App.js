8c36e97c87a772d217aa2fdebc67a22b99f32d23
richa_chachan <richa.chachan@decimal.co.in>
2024-06-22 22:52:27 +0530

    changes

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from './Component/Header';
-import Body from './Component/Body';
+import Header from "./Component/Header";
+import Body from "./Component/Body";
 
-const App = ()=>{
-    return(
-        <>
-        <Header/>
-        <Body/>
-        </>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
+root.render(<App />);
