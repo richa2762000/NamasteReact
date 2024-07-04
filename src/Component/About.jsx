68ca30649c0630ae082afe0a8ab3085998a773d6
richa_chachan <richa.chachan@decimal.co.in>
2024-07-04 17:28:27 +0530

    try

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,7 +1,11 @@
+import Restaurant from "./Restaurant/Restaurant";
+
 const About = ()=>{
     return(
         <>
-        <h2>about</h2></>
+        <h2>about</h2> 
+        <Restaurant/>
+        </>
     )
 }
 export default About;
